06a2e58794dc7e91594f821f4bf5ff39d7e2f850
Jaheed Haynes <jaheedhaynes@pursuit.org>
2020-01-15 20:46:18 -0500

    added a fetchCountry method to initial VC.

--- Pursuit-Core-iOS-Collection-View-Lab/MainViewController.swift
+++ Pursuit-Core-iOS-Collection-View-Lab/MainViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import NetworkHelper
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var countries = [Country]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
     }
 
-
+    func fetchCountry(searchQuery: String) {
+        CountryAPIClient.getCountries(searchQuery: searchQuery, completion: { [weak self] (result) in
+            switch result {
+            case .failure(let appError):
+                print("could not fetch countries with error \(appError)")
+            case .success(let countries):
+                self?.countries = countries
+            }
+        })
+    }
 }
 
